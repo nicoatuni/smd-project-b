ca6076c8258b086e5f297a3a03081fd7a7b5bcf5
Arnold Angelo <a.angelo@student.unimelb.edu.au>
2018-04-28 18:36:08 +1000

    Adding PropertyManager Class
    1. Adding PropertyManager Class to handle operation with properties file.
    2. Simulation Class uses this PropertyManager Class by utilising the getProperty method
    3. A bit of change in Simulation Class regarding static final to static (MAIL_TO_CREATE), and local variable to global variable (penalty)
    4. A bit of change in the Automail Class to handle creation of Robot because of additional String parameter from Simulation Class

--- swen30006/automail/Simulation.java
+++ swen30006/automail/Simulation.java
@@ -17,15 +17,15 @@ import java.util.Properties;
 public class Simulation {
 
     /** Constant for the mail generator */
-    private static final int MAIL_TO_CREATE = 180;
+    private static int MAIL_TO_CREATE = 180;
     
-
     private static ArrayList<MailItem> MAIL_DELIVERED;
     private static double total_score = 0;
-
+    private static double penalty = 1.1;
+    
     public static void main(String[] args) { //throws IOException {
  /*   	// Should probably be using properties here
-    	Properties automailProperties = new Properties();
+    		Properties automailProperties = new Properties();
 		// Defaults
 		automailProperties.setProperty("Name_of_Property", "20");  // Property value may need to be converted from a string to the appropriate type
 
@@ -42,7 +42,12 @@ public class Simulation {
 		
 		int i = Integer.parseInt(automailProperties.getProperty("Name_of_Property"));
 */
-
+    		PropertyManager propertymanager = new PropertyManager("automail.properties");
+    		int seed = Integer.parseInt(propertymanager.getProperty("Seed"));
+    		Building.setFloor(Integer.parseInt(propertymanager.getProperty("Number_of_Floors")));
+    		penalty = Double.parseDouble(propertymanager.getProperty("Delivery_Penalty"));
+    		MAIL_TO_CREATE = Integer.parseInt(propertymanager.getProperty("Mail_to_Create"));
+    		
         MAIL_DELIVERED = new ArrayList<MailItem>();
                 
         /** Used to see whether a seed is initialized or not */
@@ -51,12 +56,12 @@ public class Simulation {
         /** Read the first argument and save it as a seed if it exists */
         if(args.length != 0){
         	//int seed = 300;
-        int seed = Integer.parseInt(args[0]);
+        
         	seedMap.put(true, seed);
         } else{
         	seedMap.put(false, 0);
         }
-        Automail automail = new Automail(new ReportDelivery());
+        Automail automail = new Automail(new ReportDelivery(), propertymanager.getProperty("Robot_Type_1"), propertymanager.getProperty("Robot_Type_2"));
         MailGenerator generator = new MailGenerator(MAIL_TO_CREATE, automail.mailPool, seedMap);
         
         /** Initiate all the mail */
@@ -104,15 +109,15 @@ public class Simulation {
     }
     
     private static double calculateDeliveryScore(MailItem deliveryItem) {
-    	// Penalty for longer delivery times
-    	final double penalty = 1.1;
-    	double priority_weight = 0;
+    		// Penalty for longer delivery times
+    		
+    		double priority_weight = 0;
         // Take (delivery time - arrivalTime)**penalty * (1+sqrt(priority_weight))
-    	if(deliveryItem instanceof PriorityMailItem){
-    		priority_weight = ((PriorityMailItem) deliveryItem).getPriorityLevel();
+    		if(deliveryItem instanceof PriorityMailItem){
+    			priority_weight = ((PriorityMailItem) deliveryItem).getPriorityLevel();
+    		}
+    		return Math.pow(Clock.Time() - deliveryItem.getArrivalTime(),penalty)*(1+Math.sqrt(priority_weight));
     	}
-        return Math.pow(Clock.Time() - deliveryItem.getArrivalTime(),penalty)*(1+Math.sqrt(priority_weight));
-    }
 
     public static void printResults(){
         System.out.println("T: "+Clock.Time()+" | Simulation complete!");
--- swen30006/strategies/Automail.java
+++ swen30006/strategies/Automail.java
@@ -9,7 +9,7 @@ public class Automail {
     private Robot robot1, robot2;
     public IMailPool mailPool;
     
-    public Automail(IMailDelivery delivery) {
+    public Automail(IMailDelivery delivery, String robot1_action, String robot2_action) {
     	// Swap between simple provided strategies and your strategies here
     	    	
     	/** Initialize the MailPool */
@@ -21,13 +21,29 @@ public class Automail {
     	boolean weak = false;  // Can't handle more than 2000 grams
     	boolean strong = true; // Can handle any weight that arrives at the building
     	
+    	boolean roboact1;
+    	boolean roboact2;
+    	
+    	if (robot1_action.equals("weak")) {
+    		roboact1 = weak;
+    	} else {
+    		roboact1 = strong;
+    	}
+    	
+    	if (robot2_action.equals("weak")) {
+    		roboact2 = weak;
+    	} else {
+    		roboact2 = strong;
+    	}
+    	
+    	
     	//// Swap the next two lines for the two below those
     	IRobotBehaviour robotBehaviourW = new MyRobotBehaviour(weak);
     	IRobotBehaviour robotBehaviourS = new MyRobotBehaviour(strong);
     	    	
     	/** Initialize robot */
-    	robot1 = new Robot(robotBehaviourW, delivery, mailPool, weak); /* shared behaviour because identical and stateless */
-    	robot2 = new Robot(robotBehaviourS, delivery, mailPool, strong);
+    	robot1 = new Robot(robotBehaviourW, delivery, mailPool, roboact1); /* shared behaviour because identical and stateless */
+    	robot2 = new Robot(robotBehaviourS, delivery, mailPool, roboact2);
     }
     
     public Robot getRobot1() {
